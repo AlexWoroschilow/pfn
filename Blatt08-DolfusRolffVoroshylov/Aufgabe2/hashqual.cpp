47f36761e955015960c400e88adcb563d6a78965
Tim Rolff <3rolff@informatik.uni-hamburg.de>
2015-06-04 22:43:57 +0200

    Initial Commit for exercive 1 and full exercice 2

---
+++ Blatt08-DolfusRolffVoroshylov/Aufgabe2/hashqual.cpp
@@ -0,0 +1,111 @@
+#include <stdlib.h>
+
+#include <map>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+
+#include "hashqual.hpp"
+#include "tokenizer.hpp"
+#include "hashqual-functions.h"
+
+/** Definition für die Ergebnisse der Token */
+typedef std::map<unsigned int, std::vector<std::string> > ComputeSingleStore;
+/** Definition für ein paar aus ComputeSingleStore und string */
+typedef std::pair<std::string, ComputeSingleStore > StringStorePair;
+/** Definition für alle Ergebnisse der Hash funktionen */
+typedef std::vector<StringStorePair> ComputeStore;
+
+bool HashQuality::QualityOrder::operator ()(const QualityStore::value_type& a,
+                                            const QualityStore::value_type& b) {
+  /* Aufgrund von ungenauigkeiten beim Vergleichen mit
+     epsilon auf gleichheit prüfen */
+  if (std::abs(a.second - b.second) <
+      std::numeric_limits<double>::epsilon()) {
+    return a.first < b.first;
+  }
+
+  return a.second < b.second;
+}
+
+HashQuality::HashQuality() {
+  tokenizer(HashQuality::tokenHandler, this);
+  compute();
+}
+
+void HashQuality::compute() {
+  unsigned long i;
+  ComputeStore::iterator it;
+  ComputeStore computeStore;
+  const unsigned int hashCount = hashfunction_number();
+
+  // Alle Hash funktionen durchlaufen
+  for (i = 0; i < hashCount; ++i) {
+    StringSet::iterator it;
+    ComputeSingleStore singleStore;
+    const Hashfunction* func = hashfunction_get(i);
+    /* und in den jeweiligen singlestore der Funktion doppelte Werte
+       in den vektor des stores einfügen */
+    for (it = tokenStore.begin(); it != tokenStore.end(); ++it) {
+      const unsigned int hashValue = func->func(it->c_str());
+      singleStore[hashValue].push_back(*it);
+    }
+
+    computeStore.push_back(ComputeStore::value_type(func->name, singleStore));
+  }
+
+  for (it = computeStore.begin(); it != computeStore.end(); ++it) {
+    // hashqual(h,T) = Sum f(h, T, i)^2 berechnen
+    std::vector<std::string>::size_type sum = 0;
+    ComputeSingleStore::iterator single;
+    for (single = it->second.begin(); single != it->second.end(); ++single) {
+      sum += std::pow(single->second.size(), 2); //f(h, T, i)^2
+    }
+    // und das Ergebniss speichern sowie durch |H(h, T)| teilen
+    qualityStore.push_back(
+      QualityStore::value_type(it->first,
+                               static_cast<double>(sum) /
+                               static_cast<double>(it->second.size())));
+  }
+  // Sortieren der Objekte nach ihrer Qualität und dann nach dem Namen
+  std::sort(qualityStore.begin(), qualityStore.end(), QualityOrder());
+}
+
+void HashQuality::tokenHandler (const char* token, void* data) {
+  static_cast<HashQuality*>(data)->addToken(token);
+}
+
+std::ostream& operator << (std::ostream& stream,
+                           const HashQuality& qual) {
+  /** Default Output für einen stream um
+      code duplikation zu vermeiden
+  */
+  #define defaultOutput     \
+       it->first            \
+    << ": "                 \
+    << std::fixed           \
+    << std::setprecision(8) \
+    << it->second
+
+  HashQuality::QualityStore::const_iterator it;
+  stream << "# different words in input: "
+         << qual.tokenStore.size()
+         << std::endl;
+
+  for (it = qual.qualityStore.begin();
+       it != qual.qualityStore.end() - 1;
+       ++it) {
+    stream << defaultOutput << std::endl;
+  }
+
+  return stream << defaultOutput;
+
+#undef defaultOutput
+}
+
+int main() {
+  std::cout << HashQuality() << std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ Blatt08-DolfusRolffVoroshylov/Aufgabe2/hashqual.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <ostream>
+
+#include "hashqual-functions.h"
+
+class HashQuality
+{
+  friend class std::basic_ostream<char>;
+  public:
+    /** Konstruktor, dieser erstellt eine Liste mit allen möglichen
+        Hashfunction's aus hashqual-functions.h und ruft compute auf.
+        Die Ausgabe erfolgt über den standard output operator
+    */
+    HashQuality();
+   
+  private:
+    /** Definition für den Container der die Token hält */
+    typedef std::set<std::string> StringSet;
+    /** Definition für den Container welcher die berechnete
+        Qualität der Hash Funktion beinhaltet. */
+    typedef std::vector<std::pair<std::string, double> > QualityStore;
+    
+    /** Nested struct um eine Sortierordnung nach Qualität und Namen
+        zu gewährleisten. */
+    struct QualityOrder {
+      bool operator () (const QualityStore::value_type& a,
+                        const QualityStore::value_type& b);
+    };
+    
+    /** Schreibt die berechnete Hash qualität in einen ostream */
+    friend std::ostream& operator << (std::ostream& stream,
+                                      const HashQuality& qual);
+                                      
+    /** Der Tokenhandler, welcher an den tokenizer übergeben wird
+    um den gegeben input zu tokenizen.
+    Hierbei wird erwartet, dass data ein Pointer auf eine Instanz
+    von HashQuality ist. */
+    static void tokenHandler (const char* token, void* data);
+                                      
+    /** Berechnet die Hash qualität der gegebenen Hash funktionen. */
+    void compute();
+    
+    /** Fügt einen Token in den token store ein. */
+    void addToken(const std::string& tok);
+  
+    /** str_set nach vorlesung */
+    StringSet tokenStore;
+    /** Store für die berechnete Qualität */
+    QualityStore qualityStore;
+};
+
+inline void HashQuality::addToken(const std::string& tok) { 
+  /* Funktion in die Headerdatei einlagern, damit jede
+     Übersetzungseinheit die Möglichkeit hat diese
+     Funktion zu inlinen. */
+  tokenStore.insert(tok);
+}
